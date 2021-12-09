4647263d9a9df6de890d22fea933cd8318a6636b
SeRin-Yang <serin.yang95@gmail.com>
2021-12-09 01:26:05 +0100

    Change reviewed parts (2)
    - Change className according to the situation (Hover Event)

--- assignment02/src/components/ExpandableListItem/ExpandableListItem.js
+++ assignment02/src/components/ExpandableListItem/ExpandableListItem.js
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
 import './ExpandableListItem.css';
 
 function ExpandableListItem({url, title}) {
+  const [hovered, setHovered] = useState(false);
+
+  const onMouseEnter = () => setHovered(true);
+  const onMouseLeave = () => setHovered(false);
+
   return (
-    <li className='itemWrapper'>
+    <li className='itemWrapper' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <h3>
         {title}
       </h3>
-      <a href={url} className="more hide">more</a>
+      <a href={url} className={hovered ? 'more' : 'more hide'}>more</a>
     </li>
   );
 }
